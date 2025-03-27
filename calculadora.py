15a96e0c26f85dfb78fbfd822b8e66b2cb299884
Leonardo-Cirino <leonardocirino@live.com>
2025-03-26 21:38:50 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+def main():
+    print("Calculadora Simples!")
+
+    nome = input("Digite seu nome: ")
+    print(f"Olá, {nome}! Vamos realizar uma soma simples.")
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    soma = num1 + num2
+
+    print(f"{nome}, o resultado da soma é: {soma}")
+
+if __name__ == "__main__":
+    main()
