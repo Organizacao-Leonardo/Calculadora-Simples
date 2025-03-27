28a65f63e02d8e5f4795029aa248cc52800ad9cf
Leonardo-Cirino <leonardocirino@live.com>
2025-03-26 21:53:51 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,12 +7,8 @@ def main():
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
     soma = num1 + num2
-    multiplicacao = num1 * num2
-    subtracao = num1 - num2
     
     print(f"{nome}, o resultado da soma é: {soma}")
-    print(f"{nome}, o resultado da multiplicação é: {multiplicacao}")
-    print(f"{nome}, o resultado da subtração é: {subtracao}")
 
 if __name__ == "__main__":
     main()
